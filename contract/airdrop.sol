8a4c91ed11653e7f835b4ae6d08ebd1c72b7b54d
Saintt042 <103144243+Saintt042@users.noreply.github.com>
2022-04-20 15:55:10 +0100

    Update airdrop.sol

--- contract/airdrop.sol
+++ contract/airdrop.sol
@@ -34,7 +34,7 @@ contract Ownable {
 
   /**
    * @dev Allows the current owner to transfer control of the contract to a newOwner.
-   * @param newOwner The address to transfer ownership to.
+   * @saint newOwner The address to transfer ownership to.
    */
   function transferOwnership(address newOwner) onlyOwner public {
     require(newOwner != address(0));
