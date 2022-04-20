31f786aac1a2a08c9f5b017b3d888eebb13e29e8
Saintt042 <103144243+Saintt042@users.noreply.github.com>
2022-04-20 16:35:06 +0100

    Create truffle.js

---
+++ truffle.js
@@ -0,0 +1,20 @@
+var HDWalletProvider = require("truffle-hdwallet-provider-privkey");
+
+module.exports = {
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      gas: 6721975,
+      network_id: "*" // Match any network id
+    },
+    ropsten: {
+      provider: function() {
+              return new HDWalletProvider(["<Priv key goes here>"],
+                    "https://ropsten.infura.io/<Infura pub key>")
+            },
+      network_id: 3,
+      gas: 4600000
+    }
+  }
+};
